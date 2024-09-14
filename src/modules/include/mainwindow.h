e4907cb8b251b12de9804270d3052e570a4ce820
kossadda <gabilov1997@gmail.com>
2024-09-14 15:20:53 +0500

    feat: mainwindow in one header

--- src/modules/include/mainwindow.h
+++ src/modules/include/mainwindow.h
@@ -12,27 +12,55 @@
 #ifndef SRC_MODULES_INCLUDE_MAINWINDOW_H_
 #define SRC_MODULES_INCLUDE_MAINWINDOW_H_
 
-#include <QMainWindow>
+#include <QPainter>
+#include <QPainterPath>
+#include <QTabWidget>
 
 #include "modules/include/calculator_view.h"
 #include "modules/include/top_menu.h"
 
-namespace Ui {
-class MainWindow;
-}
-
-class MainWindow : public QMainWindow {
+class MainWindow : public TopMenu {
   Q_OBJECT
 
  public:
-  explicit MainWindow(QWidget *parent = nullptr);
-  ~MainWindow();
+  explicit MainWindow(QWidget *parent = nullptr)
+      : TopMenu(parent),
+        calculator{new CalculatorView{}},
+        credit{new QWidget{}},
+        deposit{new QWidget{}},
+        tab{new QTabWidget{}} {
+    setWindowIcon(QIcon{":calculator.png"});
 
-  //  protected:
-  // void paintEvent(QPaintEvent *event) override;
+    tab->addTab(credit, QIcon{":credit.png"}, QString{"Credit"});
+    tab->addTab(calculator, QIcon{":calculator.png"}, QString{"Engineer"});
+    tab->addTab(deposit, QIcon{":deposit.png"}, QString{"Deposit"});
+
+    tab->setCurrentWidget(calculator);
+
+    tab->setStyleSheet(QString{
+    "QTabWidget { background-color: rgb(255, 255, 255); }"
+    "QTabWidget::pane { border-top: 1px solid rgba(40, 100, 180, 0.7); }"
+    "QTabWidget::tab-bar { left: 22px; }"
+    "QTabBar::tab { background: rgb(40, 40, 40);"
+    "color: rgb(255, 255, 255);"
+    "border: 1px solid rgba(40, 100, 180, 0.7);"
+    "border-top-left-radius: 10px;"
+    "border-top-right-radius: 10px;"
+    "min-width: 23ex;"
+    "padding: 7px;"
+    "font-size: 13px;"
+    "font-weight: bold; }"
+    "QTabBar::tab:selected { border: 1px solid rgba(40, 100, 180, 1.0);"
+    "font-size: 14px; }"
+    "QTabBar::tab:!selected { margin-top: 10px; }"});
+
+    TopMenu::mainLayout->addWidget(tab);
+  }
 
  private:
-  Ui::MainWindow *ui;
+  QTabWidget *tab;
+  CalculatorView *calculator;
+  QWidget *credit, *deposit;
 };
 
 #endif  // SRC_MODULES_INCLUDE_MAINWINDOW_H_
--- src/modules/include/top_menu.h
+++ src/modules/include/top_menu.h
@@ -1,12 +1,12 @@
 /**
  * @file top_menu.h
  * @author kossadda (https://github.com/kossadda)
- * @brief 
+ * @brief
  * @version 1.0
  * @date 2024-09-13
- * 
+ *
  * @copyright Copyright (c) 2024
- * 
+ *
  */
 
 #ifndef SRC_MODULES_INCLUDE_TOP_MENU_H_
@@ -28,21 +28,17 @@ class TopMenu : public QWidget {
         imageLabel{new QLabel{this}},
         windowName{new QLabel{QString{"SmartCalculator"}, this}},
         grid{new QGridLayout{this}},
-        close{new QPushButton{QString{"x"}, this}},
-        collapse{new QPushButton{QString{"–"}, this}},
+        close_but{new QPushButton{QString{"x"}, this}},
+        collapse_but{new QPushButton{QString{"–"}, this}},
         space1{new QSpacerItem{0, 0, QSizePolicy::Expanding,
                                QSizePolicy::Expanding}},
         space2{new QSpacerItem{0, 0, QSizePolicy::Expanding,
                                QSizePolicy::Expanding}},
-        frame{new QFrame{this}} {
-    imageLabel->setPixmap(QPixmap{":calculator.png"});
-    imageLabel->setFixedSize(25, 25);
-    imageLabel->setScaledContents(true);
+        topframe{new QFrame{this}} {
+    close_but->setFixedSize(40, 25);
+    collapse_but->setFixedSize(40, 25);
 
-    close->setFixedSize(40, 25);
-    collapse->setFixedSize(40, 25);
-
-    close->setStyleSheet(
+    close_but->setStyleSheet(
         QString{"QPushButton:pressed {"
                 "background-color: rgb(147, 18, 21);}"
                 "QPushButton {"
@@ -51,7 +47,7 @@ class TopMenu : public QWidget {
                 "background-color: rgb(47, 47, 47);"
                 "font-size: 16px;"
                 "border: 1px solid rgba(40, 100, 180, 0.7);}"});
-    collapse->setStyleSheet(
+    collapse_but->setStyleSheet(
         QString{"QPushButton:pressed {"
                 "background-color: rgb(26, 77, 144);}"
                 "QPushButton {"
@@ -68,34 +64,63 @@ class TopMenu : public QWidget {
                 "color: rgb(130, 180, 240);"
                 "padding-left: 60px;"
                 "font-size: 16px;}"});
-    frame->setStyleSheet(
-        QString{"QFrame#frame {"
-                // "border-top-left-radius: 10px;"
-                // "border-top-right-radius: 10px;"
-                "background: rgba(0, 0, 0, 0.0);"
-                "border-bottom: 1px solid rgba(40, 100, 180, 0.7);}"});
+    topframe->setStyleSheet(
+        QString{"background: rgba(0, 0, 0, 0.0);"
+                "border-bottom: 1px solid rgba(40, 100, 180, 0.7);"});
 
     grid->addWidget(imageLabel, 0, 0);
     grid->addItem(space1, 0, 1);
     grid->addWidget(windowName, 0, 2);
     grid->addItem(space2, 0, 3);
-    grid->addWidget(collapse, 0, 4);
-    grid->addWidget(close, 0, 5);
-    frame->setLayout(grid);
-    frame->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+    grid->addWidget(collapse_but, 0, 4);
+    grid->addWidget(close_but, 0, 5);
+    topframe->setLayout(grid);
+    topframe->setFixedHeight(45);
+    topframe->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
 
-    QVBoxLayout *mainLayout = new QVBoxLayout(this);
-    mainLayout->addWidget(frame);
+    mainLayout = new QGridLayout{this};
+    mainLayout->addWidget(topframe, 0, 0);
+    mainLayout->setAlignment(topframe, Qt::AlignTop);
     mainLayout->setContentsMargins(0, 0, 0, 0);
     setLayout(mainLayout);
+
+    imageLabel->setPixmap(QPixmap{":calculator.png"});
+    imageLabel->setFixedSize(25, 25);
+    imageLabel->setScaledContents(true);
+
+    setWindowFlag(Qt::FramelessWindowHint);
+    setAttribute(Qt::WA_TranslucentBackground);
+    setFixedSize(600, 700);
+  }
+
+ protected:
+  QGridLayout *mainLayout;
+  QLabel *imageLabel, *windowName;
+
+  void paintEvent(QPaintEvent *event) {
+    Q_UNUSED(event);
+
+    QPainter painter(this);
+    painter.setRenderHint(QPainter::Antialiasing);
+
+    QRect rect = this->rect();
+    QPainterPath path;
+    path.addRoundedRect(rect, 10, 10);
+
+    painter.fillPath(path, QBrush(QColor{10, 10, 10, 220}));
+
+    QPen pen(QColor(40, 100, 180, 178), 2);
+    painter.setPen(pen);
+    painter.drawPath(path);
+
+    QWidget::paintEvent(event);
   }
 
  private:
-  QLabel *imageLabel, *windowName;
   QGridLayout *grid;
-  QPushButton *close, *collapse;
+  QPushButton *close_but, *collapse_but;
   QSpacerItem *space1, *space2;
-  QFrame *frame;
+  QFrame *topframe;
 };
 
 #endif  // SRC_MODULES_INCLUDE_TOP_MENU_H_
